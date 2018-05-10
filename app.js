72188caf121e8b7a25534aeb00b92bc1914b6ba9
Eduardo de Brito Colombo <eduardobcolombo@gmail.com>
2018-05-10 15:05:20 -0300

    Setup MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 4000;
+
+mongoose.connect("mongodb://graphql:graphql@ds119930.mlab.com:19930/graphql-learning");
+mongoose.connection.once('open', () => {
+    console.log('Connected to database!');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
