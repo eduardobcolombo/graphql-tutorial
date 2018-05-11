a8b18824050d55dfbec9c99a2a1d6078b1adde62
Eduardo de Brito Colombo <eduardobcolombo@gmail.com>
2018-05-11 10:59:46 -0300

    Added the AddBook Layout

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -16,13 +16,14 @@ class BookList extends Component {
     var data = this.props.data;
     if (data.loading) {
       return(<div>Loading books...</div>);
-    } else {
-      return data.books.map(book => {
-        return (
-          <li key={book.id}>{ book.name }</li>
-        );
-      });
     }
+    
+    return data.books.map(book => {
+      return (
+        <li key={book.id}>{ book.name }</li>
+      );
+    });
+    
   }
   render() {
     return (
