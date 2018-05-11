96014647b5bef2c83eaf94628994476242df83de
Eduardo de Brito Colombo <eduardobcolombo@gmail.com>
2018-05-11 17:01:34 -0300

    Added the action AddBook on submit

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -2,10 +2,10 @@ import { gql } from 'apollo-boost';
 
 const getBooksQuery = gql`
 {
-    books{
-        name
-        id
-    }
+  books{
+    name
+    id
+  }
 }
 `
 
@@ -18,4 +18,13 @@ const getAuthorsQuery = gql`
 }
 `
 
-export {getAuthorsQuery, getBooksQuery};
+const addBookMutation = gql`
+  mutation($name:String!, $genre:String!, $authorId:ID!) {
+    addBook(name: $name, genre: $genre, authorId: $authorId){
+      name
+      id
+    }
+  }
+`
+
+export {getAuthorsQuery, getBooksQuery, addBookMutation};
