2fd91686f6d63e95ada5f42cf7f6c24fa01b267b
Eduardo de Brito Colombo <eduardobcolombo@gmail.com>
2018-05-11 09:50:21 -0300

    Added Cors

--- server/app.js
+++ server/app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const port = 4000;
+
+// allow cors-origin requrests
+app.use(cors());
 
 mongoose.connect("mongodb://graphql:graphql@ds119930.mlab.com:19930/graphql-learning");
 mongoose.connection.once('open', () => {
